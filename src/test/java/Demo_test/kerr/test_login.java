3d18415f441716e753c9ba31882f2b26931586a5
Administrator <Administrator@192.168.167.32>
2018-03-26 11:37:24 +0800

    测试提交

---
+++ src/test/java/Demo_test/kerr/test_login.java
@@ -0,0 +1,30 @@
+package Demo_test.kerr;
+
+import org.openqa.selenium.By;
+//import org.openqa.selenium.By.ById;
+//import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+//import java.lang.*;
+import lib.login_lib;
+
+public class test_login {
+	@BeforeMethod
+	public void set() {
+		System.setProperty("webdriver.firefox.bin","D:/Program Files/Mozilla Firefox/Firefox.exe");
+	}
+	@Test
+	public void getUrl() throws InterruptedException {
+		WebDriver driver=new FirefoxDriver();
+		driver.get(login_lib.url);
+		Thread.sleep(1000);
+		driver.findElement(By.id(login_lib.player)).sendKeys(login_lib.account);
+		driver.findElement(By.id(login_lib.pwds)).sendKeys(login_lib.password);
+		driver.findElement(By.name(login_lib.in_btn)).click();
+		Thread.sleep(2000);
+		driver.quit();
+	}
+}
