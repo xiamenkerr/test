15c778b05dd6f999a1d5bfe428843b5e5d33bfd6
kerr <Administrator@192.168.167.32>
2018-03-26 11:59:22 +0800

    login_lib

---
+++ src/main/java/lib/login_lib.java
@@ -0,0 +1,11 @@
+package lib;
+
+public interface login_lib {
+	String url="http://sitcgadmin.fixed-stars.com/S01/login/showLogin.do";
+	String player="adminName";
+	String pwds="pwd";
+	String in_btn="登陆";
+	String account="admin2";
+	String password="aaaaaa";
+	String baidu="https://www.baidu.com/";
+}
