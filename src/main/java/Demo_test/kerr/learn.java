93d0ed5d083af47c07e0dbbd5088c8be479823b7
Administrator <Administrator@192.168.167.32>
2018-03-26 11:53:59 +0800

    项目同步

---
+++ src/main/java/Demo_test/kerr/learn.java
@@ -0,0 +1,30 @@
+package Demo_test.kerr;
+
+import org.testng.annotations.*;
+
+public class learn {
+	
+/*	@Test(groups="submodule1")
+	public void test1() {
+		System.out.println("这是Test1");
+	}
+	
+	@Test(groups="submodule2")
+	public void test2() {
+		System.out.println("这是Test2");
+	}
+	
+	@Test(groups="submodule3")
+	public void test3() {
+		System.out.println("这是Test3");
+	}*/
+	
+	@Parameters({"testdate1"})
+	@Test
+	public void test(@Optional("我是默认的参数") String testdate) {
+		System.out.println(testdate);
+	}
+	
+	
+}
+
---
+++ src/main/java/lib/CreatMerchant_xpath.java
@@ -0,0 +1,11 @@
+package lib;
+
+public interface CreatMerchant_xpath {
+	String add_btn="//a[contains(.,'新增商户')]";
+	String merchantName="merchantName";
+	String adminName="adminName";
+	String coinsScale="coinsScale";
+	String phone="phone";
+	String email="email";
+	String savebut="savebut";
+}
